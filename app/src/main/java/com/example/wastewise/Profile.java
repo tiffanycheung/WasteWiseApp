090cf82700eee910a9940c837cfa427722b1b350
tffnycheung@gmail.com <tffnycheung@gmail.com>
2023-11-03 01:41:37 +1100

    leaderboard complete

--- app/src/main/java/com/example/wastewise/Profile.java
+++ app/src/main/java/com/example/wastewise/Profile.java
@@ -16,9 +16,10 @@ public class Profile extends AppCompatActivity {
 
 
         // TODO: put the below code in the onClick method for the digital card (to track where the back button in the digital card page should go)
-//        Intent intent = new Intent(Profile.this, DigitalCard.class);
-//        // to track previous page
-//        intent.putExtra("PREVIOUS_PAGE", "Profile");
-//        startActivity(intent);
+        Intent intent = new Intent(Profile.this, DigitalCard.class);
+        // to track previous page
+        intent.putExtra("PREVIOUS_PAGE", "Profile");
+        intent.putExtra("PAGE_USER_CAME_FROM", "Profile");
+        startActivity(intent);
     }
 }
